361923b0bb18f2a1fd72c613659f490487ebcf81
Marcin Szulc <maszudev@gmail.com>
2017-08-12 00:52:11 +0200

    Add current student localisation setter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,7 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-    # current_user.try(:locale) || 
+    I18n.locale = current_student.try(:locale) || ( params[:locale] || I18n.default_locale )
   end
 
   def default_url_options
