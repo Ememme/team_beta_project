ee4e0a91d349929638108b825232d366500afa31
Marcin Szulc <maszudev@gmail.com>
2017-08-13 13:27:51 +0200

    Add associations and validations scpes to students and rooms

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "when is associate" do
+    it { should have_many(:tenancy_contracts) }
+    it { should have_many(:students).through(:tenancy_contracts) }
+  end
+
+  context 'when is validate' do
+    it { should validate_presence_of(:capacity) }
+    it { should validate_presence_of(:number) }
+    it { should validate_uniqueness_of(:number) }
+
+    it { should validate_length_of(:name).is_at_least(5).is_at_most(150)}
+  end
 end
