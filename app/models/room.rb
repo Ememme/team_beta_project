7b73f220056890d148e7652e06206f75b4cd4180
Marcin Szulc <maszudev@gmail.com>
2017-08-13 13:26:30 +0200

    Correct associations and add validations to students and rooms

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,8 @@
 class Room < ApplicationRecord
-  has_many :tenancy_agreements
-  has_many :students, through: :tenancy_agreements
+  has_many :tenancy_contracts
+  has_many :students, through: :tenancy_contracts
 
-  validates :capacity, :number, presence: true
+  validates :capacity, presence: true
+  validates :number, presence: true, uniqueness: true
+  validates :name, length: { minimum: 5, maximum: 150}
 end
